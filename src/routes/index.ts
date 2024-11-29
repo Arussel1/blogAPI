a58fe4387ddd3795128a08e935b661f17691c3bd
Arussel <anhminh05102004@gmail.com>
2024-11-28 22:24:37 -0500

    Backend API for blog

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import {  admin, verifyToken, isAdmin } from './../controllers/admin';
-import { user } from './../controllers/user';
 import { post, comment } from './../controllers/post';
 const router = express.Router();
 
@@ -9,19 +8,9 @@ router.post('/admin/login', cors(), admin.login);
 
 router.post('/admin/signup', cors(), admin.create);
 
-router.post('/users/signup', cors(), user.create);
-  
-router.post('/users/login', cors(), user.login);
+router.get('/posts', cors(),  post.list);
 
-router.get('/users/:userId', cors(), verifyToken, user.getInfo);
-
-router.put('/users/:userId', cors(), verifyToken, user.update);
-
-router.delete('/users/:userId', cors(), verifyToken, user.delete);
-
-router.get('/posts', cors(), verifyToken, post.list);
-
-router.get('/posts/:postId', cors(), verifyToken, post.listSpecific);
+router.get('/posts/:postId', cors(),  post.listSpecific);
 
 router.post('/posts', cors(), verifyToken, isAdmin, post.new);
 
